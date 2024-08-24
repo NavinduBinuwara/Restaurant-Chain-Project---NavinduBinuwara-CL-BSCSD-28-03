e0c47f18724d06a68d03ded0ec5e576d6c61c2f7
Navindu Binuwara <60029280+NavinduBinuwara@users.noreply.github.com>
2024-08-24 12:10:14 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const url =process.env.MONGODB || "";
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb+srv://gpnbinuwara:8ynPyyoZ690poSjn@cluster0.cx68e.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+   
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
