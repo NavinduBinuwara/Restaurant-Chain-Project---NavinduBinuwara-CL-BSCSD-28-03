4b1d4f2680f9f8b3eece2c57d50a223a082d7ef7
NavinduBinuwara <gpn.binuwara@gmail.com>
2024-08-24 13:29:07 +0530

    initial styles changes home page - Navindu

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>When you dine at Binu Restaurant, you're not just enjoying a meal – you're embarking on a culinary journey. Our carefully curated menu offers something for everyone, from classic dishes that remind you of home to bold new flavors that excite the palate. Whether you're here for a casual lunch, a romantic dinner, or a special celebration, Binu Restaurant is the perfect setting.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+94 0765832784</li>
+                <li>contact@binurestaurant.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © Binurestaurant.com - All Right Reserved.</p>
     </div>
   )
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <p>At Binu Restaurant, we believe that every meal should be an experience to remember. Nestled in the heart of colombo, Binu Restaurant is where culinary passion meets creativity. Our mission is to bring people together over delicious food and create a warm, inviting atmosphere that feels just like home.</p>
                 <button>View Menu</button>
             </div>
         </div>
